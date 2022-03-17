4f74b45d02d1a48316968c44fa5628fa63ec788e
konok101 <rockykonok@gmail.com>
2022-03-18 05:21:12 +0600

    switch part done

---
+++ switch.js
@@ -0,0 +1,18 @@
+var color= 'red';
+ 
+ 
+
+switch(color){
+    case 'yellow':
+        console.log('This is the yellow');
+        break;
+
+        case 'white':
+   
+            console.log('This is thr white part');
+            break;
+
+        case 'red':
+            console.log('this is red part');
+            break
+}
