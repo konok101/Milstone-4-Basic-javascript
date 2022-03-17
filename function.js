b3aba6f313785c3bdf41fd859115f5c4a9f8360e
konok101 <rockykonok@gmail.com>
2022-03-18 04:45:50 +0600

    function part done

---
+++ function.js
@@ -0,0 +1,44 @@
+
+// Declare a function
+// function declare(){
+//     console.log('Declare th function rule ')
+// }
+// declare();
+
+
+
+
+
+// use parameter for a function
+
+// function add(num1, num2){
+//     const c= num1+num2;
+//     console.log(c);
+// }
+// add(22, 28);
+
+
+
+
+// use another parameter for a function
+// function addTwo(e, f){
+//     var c= e+f;
+//     console.log(c);
+
+// }
+// var a=21;
+// var b=29; 
+// addTwo(a, b);
+
+
+ 
+// use function to Return type
+
+function returnType(c,d){
+    return c+d;
+
+}
+const value= returnType(22,21);
+
+console.log(value);
+ 
---
+++ object.js
@@ -0,0 +1,22 @@
+
+// Declare a object
+var computer ={
+    id: 323,
+    color: 'red',
+    price: 29000,
+    prosesor: '256gb'
+
+};
+
+console.log('computer ID= ',computer.id);
+
+var computerColor= computer.color;
+console.log('Computer color ',computerColor);
+
+// set a value object property change
+computer.price= 300000;
+
+console.log('computer price ', computer.price)
+
+
+
