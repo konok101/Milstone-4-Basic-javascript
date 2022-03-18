83c1a745abc48330b38c43982341f09fd2fb7977
konok101 <rockykonok@gmail.com>
2022-03-19 03:43:03 +0600

    odd even function use

---
+++ add-even-function.js
@@ -0,0 +1,20 @@
+ 
+function checkNumber(number){
+    if(number%2==0){
+        return true;
+    }
+    return false;
+}
+let checkOdd=21;
+
+let oddNum= checkNumber(checkOdd);
+
+console.log("Check odd number ",oddNum);
+
+
+let CheckEven=44;
+let evenNum= checkNumber(CheckEven);
+console.log('check even number ', evenNum);
+
+
+ 
